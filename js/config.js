9ba8f687fe6d22199a3f1111f4b2fd12bf7826ea
udith <uditharosha@gmail.com>
2019-06-27 05:12:00 +0000

    Added user pool configs

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_l5siP41Gk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6bidj4alip2kef4ubl7v22qbja', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
